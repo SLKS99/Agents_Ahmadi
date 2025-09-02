3a15fce39c9227c382e33f06fdb6d66cf7ea34cf
CyberA183 <aa13.thompson@icloud.com>
2025-09-02 12:54:21 -0400

    Fixed script reading wavelengths

--- tools/script.py
+++ tools/script.py
@@ -15,14 +15,14 @@ from .instruct import FITTING_SCRIPT_CORRECTION_INSTRUCTIONS, FITTING_SCRIPT_GEN
 class CurveFitting:
 
     MAX_SCRIPT_ATTEMPTS = 3
-    LUM_READ_NUMBERS = ('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,'
-                        '33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,'
-                        '62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,'
-                        '91,92,93,94,95,96,97,98,99,100')
+    LUM_READ_NUMBERS = """1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,"
+                        33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,
+                        "62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
+                        "91,92,93,94,95,96,97,98,99,100"""
 
     def __init__(self, output_dir: str = "curve_fitting", executor_timeout: int = 60, wells_to_ignore: str = "",
                  start_wavelength: int = 500, end_wavelength: int = 850, wavelength_step_size: int = 1, time_step: int = 100,
-                 number_of_reads: int = 100, luminescence_read_numbers: list = LUM_READ_NUMBERS):
+                 number_of_reads: int = 100, luminescence_read_numbers: str = LUM_READ_NUMBERS):
          self.luminescence_read_numbers = luminescence_read_numbers
          self.wells_to_ignore = wells_to_ignore
          self.number_of_reads = number_of_reads
@@ -73,7 +73,7 @@ class CurveFitting:
 
         for i in range(1, number_of_reads + 1):
             rows += data[data[data.columns[0]] == "Read " + str(i) + ":EM Spectrum"].index.tolist()
-            rows += data[data[data.columns[0]] == "Results"].index.tolist()
+        rows += data[data[data.columns[0]] == "Results"].index.tolist()
 
         #Seperate into different dataframes
 
